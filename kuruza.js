f0c6ee66ba47ce3414557d3a0ffab2f35b93dbe3
app-bonic <app.bonic@gmail.com>
2025-02-03 14:52:03 +0100

    Add files via upload

---
+++ kuruza.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCIPKzSJ2furGV5BBHqvxE2KS5I8R2of_E",
+    authDomain: "kongres-app.firebaseapp.com",
+    projectId: "kongres-app",
+    storageBucket: "kongres-app.appspot.com",
+    messagingSenderId: "965427055352",
+    appId: "1:965427055352:web:f7ec9b7e49803940f5890d"
+};
